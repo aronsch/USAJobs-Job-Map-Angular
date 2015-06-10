2edfe2ad6c9097dc2db14a267573a4a8c1cc2a50
Aron Schneider <aronsch@gmail.com>
2015-06-10 16:54:48 -0400

    Remove geocoding settings, add geocoding attribution

--- app/UsaJobsApp.Location.js
+++ app/UsaJobsApp.Location.js
@@ -5,7 +5,7 @@
  *         - Geodata Caching Service
  */
 (function () {
-	angular.module('UsaJobsApp.Location', []);
+	angular.module('UsaJobsApp.Location', ['UsaJobsApp.Settings']);
 	
 	/* Service Declaration */
 	angular.module('UsaJobsApp.Location').factory('JobLocation', JobLocationFactory);
@@ -102,12 +102,23 @@
 	 * Queues and processes geocoding requests. Designed to accomodate rate limits of geocoding services.
 	 * Geodata is cached and used whenever possible.
 	 */
-	geocodeService.$inject = [ '$http', '$timeout', 'geodataCache' ];
-	function geocodeService ($http, $timeout, geodataCache) {
-		// Standard query generation and data normalization
-		// functions for geocoding services.
-		// TODO: Clean up legacy geocoding api options
-		this.geocode = function geocode (location) {
+	geocodeService.$inject = [ '$http', '$timeout', 'geodataCache', 'settings', 'eventService' ];
+	function geocodeService ($http, $timeout, geodataCache, settings, events) {
+
+		/* Public Properties */
+		this.queue = []; // geocode processing queue
+		this.isRunning = false; // current processing status
+		this.attributionEmitted = false; // geocode service attribution status
+		
+		/* Public Function Bindings */
+		this.geocode = geocode;
+		this.addToQueue = addToQueue;
+		this.run = run;
+		this.geocodeRun = geocodeRun;
+		
+		/* Functions */
+		
+		function geocode (location) {
 			var geodata;
 			// check if location data is cached
 			if ((geodata = geodataCache.getLocation(location.name))) {
@@ -117,163 +128,57 @@
 				// otherwise, queue for geocoding
 				this.addToQueue(location);
 			}
-		};
-		this.api = {
-			/*
-			 * Return the current geocoding service API. Checks for
-			 * user-selected of built-in service, then user-provided API
-			 * interface. Defaults to Google geocoding service.
-			 */
-			currentAPI : function currentAPI () {
-				return this.custom || this[this.defaultAPI];
-			},
-			defaultAPI : "google",
-			// Route query constructor call to current API.
-			query : function query (location) {
-				var api = this.currentAPI();
-				this.attributionControl.attribute(api);
-				return api.query(location.name);
-			},
-			// Route response normalization call to current API.
-			normalizeResponse : function normalizeResponse (response) {
-				return this.currentAPI().normalizeResponse(response);
-			},
-			// Get rate limit for geocoding requests, default to 125ms
-			rateLimit : function rateLimit (response) {
-				return this.currentAPI().rateLimit || 190;
-			},
-			attributionControl : {
-				attributed : false,
-				attribute : function (api) {
-					// Add service attribution if geocoding request is made
-					// TODO: angularize custom attribution
-				}
-			},
-			geonames : {
-				name : 'GeoNames',
-				infoURL : 'http://www.geonames.org/export/geonames-search.html',
-				query : function (locationName) {
-					var query;
-					query = 'http://api.geonames.org/search';
-					query += "?q=" + locationName;
-					query += "&maxRows=1";
-					query += "&username=" + options.geocodeAPIKey;
-					query += "&type=json";
-					return encodeURI(query);
-				},
-				normalizeResponse : function (response) {
-					if (response.geonames[0]) { return {
-						lat : response.geonames[0].lat,
-						lon : response.geonames[0].lng
-					}; }
-				}
-			},
-			google : {
-				name : 'Google',
-				infoURL : 'https://developers.google.com/maps/documentation/geocoding/',
-				query : function (locationName) {
-					var query;
-					query = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-					query += locationName;
-					return encodeURI(query);
-				},
-				normalizeResponse : function (response) {
-					if (response.results[0]) {
-						response = response.results[0].geometry.location;
-						return {
-							lat : response.lat,
-							lon : response.lng
-						};
-					}
-				}
-			},
-			mapquest : {
-				name : 'MapQuest',
-				infoURL : 'http://www.mapquestapi.com/geocoding/',
-				query : function (locationName) {
-					var query;
-					query = 'http://www.mapquestapi.com/geocoding/v1/address?';
-					query += 'location=' + locationName;
-					query += '&outFormat=json';
-					query += '&thumbMaps=false';
-					query += '&maxResults=1';
-					query += '&key=' + options.geocodeAPIKey;
-					return encodeURI(query);
-				},
-				normalizeResponse : function (response) {
-					// TODO: validate response object.
-					return {
-						lat : response.latLng.lat,
-						lon : response.latLng.lng
-					};
-				}
-			},
-			bing : {
-				name : 'Bing',
-				infoURL : '',
-				query : function (locationName) {
-					var query;
-					query = 'http://dev.virtualearth.net/REST/v1/Locations';
-					query += '?addressLine=' + locationName;
-					query += 'maxResults=1';
-					query += '&key=' + options.geocodeAPIKey;
-					
-					return encodeURI(query);
-				},
-				normalizeResponse : function (response) {
-					// TODO: validate response object.
-					return {
-						lat : response.resources.point.coordinates[0],
-						lon : response.resources.point.coordinates[1]
-					};
-				}
+			
+			if (!this.attributionEmitted) {
+				emitAttribution();
 			}
-		// ,
-		// If a user-provided query and normalization function
-		// object has been provided, place it here.
-		// custom : (function () { return options.geocodingAPI; })()
 		};
-		// The geocoding queue
-		this.queue = [];
-		this.addToQueue = addToQueue;
+		
+		function emitAttribution () {
+			var str = '<a href="' + settings.geocoding.infoURL + '" ';
+			str += 'title="Job locations plotted using ' + settings.geocoding.name + ' Geocoding API" ';
+			str += 'target="_blank">' 
+			str += settings.geocoding.name;
+			str += '</a>';
+			events.location.setAttribution(str);
+			this.attributionEmitted = true;
+		}
 		
 		function addToQueue (loc) {
 			this.queue.push(loc);
-			this.status.requested();
 			this.run();
 		}
 		
-		// Process the geocoding queue. Queue processing allows us
-		// to rate-limit our calls to comply with API guidelines
-		this.run = run;
-		this.isRunning = false;
-		
+		// Process the geocoding queue. Calls are rate-limited
+		// to comply with API guidelines
 		function run (advanceQueue) {
 			var scope = this;
-			// dispatch first geocode if queue processing has not started.
-			// if the queue is progress, only dispatch next geocode if the
-			// function is
-			// called from the rate limiter with advanceQueue=true.
+			// Dispatch first geocode if queue processing has not started.
+			// If the queue is processing, only dispatch next geocode if the
+			// function is called from the rate limiter with advanceQueue=true.
 			if (!scope.isRunning || scope.isRunning && advanceQueue) {
 				scope.isRunning = true;
-				
+
 				if (scope.queue.length > 0) {
 					$timeout(function () {
 						scope.geocodeRun();
 						scope.run(true);
-					}, scope.api.rateLimit());
+					}, settings.geocoding.rateLimit);
 				} else {
 					scope.isRunning = false;
 				}
 			}
 		}
 		
-		this.geocodeRun = geocodeRun;
-		
 		function geocodeRun () {
-			var scope = this, location = scope.queue.shift(), geodata = {};
-			geodata.$promise = $http.get(scope.api.query(location));
-			geodata.$promise.success(function (data, status, q) {
+			var scope = this, // closure reference for callbacks
+			    location = scope.queue.shift(),
+			    geodata = {};
+			
+			// Request geocoding and store reference to promise object
+			geodata.promise = $http.get(settings.geocoding.query(location.name));
+			// Success
+			geodata.promise.success(function (data, status, q) {
 				var geoLoc;
 				
 				if (data.error_message && data.status === "OVER_QUERY_LIMIT" ) {
@@ -289,39 +194,20 @@
 
 				} else {
 					// set the location's geodata if there are no errors
-					geoLoc = scope.api.normalizeResponse(data);
+					geoLoc = settings.geocoding.normalizeResponse(data);
 					geoLoc.name = location.name;
 					angular.extend(geodata, geoLoc);
 					location.setGeodata(geodata);
 					geodataCache.addLocations(geoLoc);
 				}
 			});
-			geodata.$promise.error(function (data) {
+			// Error
+			geodata.promise.error(function (data) {
 				console.error(data);
 			});
-			geodata.$promise.then(function () {
-				scope.status.returned();
-			});
 		}
-		
-		// Geocoding queue status tracker
-		// TODO: Timeout error handling
-		this.status = {
-			requested : function () {
-				this.requestCount += 1;
-				
-			},
-			returned : function () {
-				this.returnedCount += 1;
-				// TODO: See if this status tool is still needed.
-			},
-			requestCount : 0,
-			returnedCount : 0,
-			percentage : function () {
-				return Math.round((this.returnedCount / this.requestCount * 100));
-			}
-		};
 	}
+	
 	
 	/**
 	 * Geodata Caching Service
